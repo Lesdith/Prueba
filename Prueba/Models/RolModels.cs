d79fa347918a10b55a9229b7c51f3a569ea27015
Lesdith <33888433+Lesdith@users.noreply.github.com>
2018-04-27 13:09:07 -0600

    Prueba de commit a rama (#2)

---
+++ Prueba/Models/RolModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Prueba.Models
+{
+    public class RolModels
+    {
+        [Key]
+        public int IDRol { get; set; }
+
+        [Required (ErrorMessage = "El campo {0} es requerido")]
+        [Display (Name = "Descripción")]
+        public string Description { get; set; }
+    }
+}
